17d7597328833a5bbde2f2079cfef60dd91d1792
Gs-ygc <gs2ygc@gmail.com>
2026-01-21 17:26:36 +0800

    Remove recursive flag for submodule initialization

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@
 # This script will setup XiangShan develop environment automatically
 
 # Init submodules
-git submodule update --init --recursive DRAMsim3 NEMU NutShell nexus-am qemu
-git submodule update --init --recursive --depth 1 llvm-project-ame
+git submodule update --init DRAMsim3 NEMU NutShell nexus-am qemu
+git submodule update --init --depth 1 llvm-project-ame
 cd nexus-am && git lfs pull; cd -;
 git submodule update --init XSAI && make -C XSAI init;
 
