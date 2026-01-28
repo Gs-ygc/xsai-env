41be94df26868b4784ac6333aa3c91821abe19c0
Gs-ygc <gs2ygc@gmail.com>
2026-01-28 18:25:47 +0800

    fix: build error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,23 @@
 # This script will setup XiangShan develop environment automatically
 
 # Init submodules
-git submodule update --init DRAMsim3 NEMU NutShell nexus-am riscv-matrix-spec qemu
-git submodule update --init --depth 1 llvm-project-ame
-cd nexus-am && git lfs pull; cd -;
-git submodule update --init XSAI && make -C XSAI init;
-
 # Setup XiangShan environment variables
+
+dev(){
+    git submodule update --init DRAMsim3 NEMU NutShell nexus-am riscv-matrix-spec qemu
+    git submodule update --init --depth 1 llvm-project-ame
+    cd nexus-am && git lfs pull; cd -;
+    git submodule update --init XSAI && make -C XSAI init;
+    cd firmware && make init; cd -;
+}
+user(){
+    git submodule update --init qemu
+    cd firmware && make init; cd -;
+}
+user
 source $(dirname "$0")/../env.sh
 # OPTIONAL: export them to .bashrc
-
 echo XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
 echo NEMU_HOME: ${NEMU_HOME}
 echo AM_HOME: ${AM_HOME}
 echo NOOP_HOME: ${NOOP_HOME}
-
